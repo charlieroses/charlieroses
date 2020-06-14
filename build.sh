036f6bb8cc60a4fa4f7324d68607057c7cc74246
Charlie Chiccarine <src322@drexel.edu>
2020-06-14 00:41:18 -0400

    Completing making the blog

--- build.sh
+++ build.sh
@@ -43,41 +43,70 @@ do
 	echo "${HTMLPAGE}" > ${OUT_FILE}
 done
 
+echo "Completed main HTML files from Markdown"
 
 if [ ! -e "${BLOGPOST_SRC}" ]
 then
 	echo "Building blogpost directory..."
 	mkdir ${BLOGPOST_SRC}
+	echo "Completed making blogpost directory"
 fi
 
 echo "Building blog HTML files from Markdown..."
+echo "Building blog main page HTML file..."
 
-for IN_FILE in ${BLOGMD_SRC}/*
+BLOG_CONTENT=" "
+
+for IN_FILE in $(ls ${BLOGMD_SRC} | sort -r)
 do
 	# Only build the page if it's not in progress
 	if [ "${IN_FILE}" = "${IN_FILE%wip*}" ]
 	then
-		OUT_FILE=${BLOGPOST_SRC}${IN_FILE#${BLOGMD_SRC}}
+		OUT_FILE=${BLOGPOST_SRC}/${IN_FILE#${BLOGMD_SRC}}
 		OUT_FILE=${OUT_FILE%.md}.html
 
 		echo "Building ${OUT_FILE} from ${IN_FILE}..."
 
-		TITLE=`head -n 1 ${IN_FILE}`
+		TITLE=`head -n 1 ${BLOGMD_SRC}/${IN_FILE}`
 		TITLE=${TITLE#"# "}
 		
 		HEADER=" - ${HEADERS[blog.html]}"
 
-		CONTENT=$(pandoc --preserve-tabs ${IN_FILE})
+		CONTENT=$(pandoc --preserve-tabs ${BLOGMD_SRC}/${IN_FILE})
 
 		HTMLPAGE=${TEMPLATE}
 		HTMLPAGE="${HTMLPAGE/<!-- HEADING -->/${HEADER}}"
 		HTMLPAGE="${HTMLPAGE/<!-- CONTENT -->/${CONTENT}}"
 
 		echo "${HTMLPAGE}" > ${OUT_FILE}
+
+		DATE=${IN_FILE%_*}
+		MONTH=${DATE%-*}
+		MONTH=${MONTH#*-}
+		DAY=${DATE##*-}
+		YEAR=${DATE%%-*}
+		PRETTYDATE="${MONTH}/${DAY}/${YEAR}"
+
+		ENTRY_LINK="<a href='${OUT_FILE#./}'>$PRETTYDATE : $TITLE</a>"
+		BLOG_CONTENT="${BLOG_CONTENT}<br>${ENTRY_LINK}"
+
 	else
 		echo "Ignoring work in progress file ${IN_FILE}"
 	fi
-
 done
 
-echo "Completed"
+echo "Completed blog HTML files from Markdown"
+
+echo "Compiling main blog.html file..."
+
+OUT_FILE="blog.html"
+
+HTMLPAGE=${TEMPLATE}
+HTMLPAGE="${HTMLPAGE/<!-- HEADING -->/${HEADER}}"
+HTMLPAGE="${HTMLPAGE/<!-- CONTENT -->/${BLOG_CONTENT}}"
+
+echo "${HTMLPAGE}" > ${OUT_FILE}
+
+echo "Completed main blog page"
+
+echo "Completed Script"
